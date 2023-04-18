88e66f3dc1f6d49add13e2c67d1ba30cb0c26a18
Ruslan Nigmetullaev <rnigmetullaev@rutube.ru>
2023-04-18 19:09:35 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const dataItems = {
+  "items": [
+    {
+      "id": 1,
+      "title": "FLOLAND",
+      "description": "Шампунь для волос питательный с азиатской центеллой Daily Nourishing Shampoo",
+      "price": 690,
+      "img_src": "https://am-beauty.ru/upload/iblock/692/7y0zkvaeqscjt2hefutqhv2jk3lb4zvj.png"
+    },
+    {
+      "id": 2,
+      "title": "HADAT",
+      "description": "Шампунь для волос глубоко очищающий Hydro Mud Hair Shampoo",
+      "price": 4554,
+      "img_src": "https://am-beauty.ru/upload/iblock/3ff/tug8mskxasfj6spgwct45fgcmlzjikt8.png",
+    },
+    {
+      "id": 3,
+      "title": "MASIL",
+      "description": "Шампунь от перхоти с яблочным уксусом 5 Probiotics Apple Vinegar Shampoo (300 мл)",
+      "price": 1300,
+      "img_src": "https://am-beauty.ru/upload/iblock/c62/7kjzsael2ze08fr693kqh4j62h2dzsit.png"
+    },
+    {
+      "id": 4,
+      "title": "HEMPZ",
+      "description": "Шампунь для волос растительный для придания объёма Sweet Pineapple & Honey Melon Volumising (265 мл)",
+      "price": 2690,
+      "img_src": "https://am-beauty.ru/upload/iblock/d02/97ij5w2tjxtomzr1ukj7u6can6ks205i.png"
+    },
+    {
+      "id": 5,
+      "title": "HADAT",
+      "description": "Набор для востановления волос Hydro Repair Hair Set",
+      "price": 5060,
+      "img_src": "https://am-beauty.ru/upload/iblock/cf8/ra22bn44dl4fbx1yiovoq73aifj9hyf9.png"
+    }
+  ]
+};
